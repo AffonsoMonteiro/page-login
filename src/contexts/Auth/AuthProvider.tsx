e47bacc707910360018cc7fbc1669b208d4d646b
AffonsoMonteiro <afonsomonteiro_@hotmail.com>
2022-02-21 21:51:01 -0300

    update

--- src/contexts/Auth/AuthProvider.tsx
+++ src/contexts/Auth/AuthProvider.tsx
@@ -1,5 +1,6 @@
 
 import { useState } from "react";
+import { useApi } from "../../hooks/useApi";
 import { User } from "../../types/User";
 import { AuthContext } from "./AuthContext";
 
@@ -7,17 +8,24 @@ import { AuthContext } from "./AuthContext";
 export function AuthProvider({ children }: {children: JSX.Element}) {
 
   const [user, setUser] = useState<User | null>(null)
+  const api = useApi()
 
-  function signin(email: string, password: string) {
-
+  async function signin(email: string, password: string) {
+    const data = await api.signin(email, password)
+    if(data.user && data.token) {
+      setUser(data.user)
+      return true;
+    }
+    return false
   }
 
-  function singout() {
-    
+  async function signout() {
+    await api.logout()
+    setUser(null)
   }
 
   return (
-    <AuthContext.Provider value={{user, signin, singout}}>
+    <AuthContext.Provider value={{user, signin, signout}}>
       {children}
     </AuthContext.Provider>
   )
